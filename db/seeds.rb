01052ab64b9a39869a471781d38d9755b911d4f3
Ridwan Hady <ridwanhady31@gmail.com>
2020-03-23 13:18:14 +0700

    Project Init

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+department1 = Department.create(name: "Computer Science")
+department2 = Department.create(name: "Electrical Engineering")
+department3 = Department.create(name: "Biomedical Engineering")
+
+student1 = Student.create(first_name: "John", last_name: "Doe", department_id:  department1.id)
+student2 = Student.create(first_name: "Alexander", last_name: "Bell", department_id:  department1.id)
+student3 = Student.create(first_name: "Albert", last_name: "Einstein", department_id:  department1.id)
+student4 = Student.create(first_name: "Jane", last_name: "Doe", department_id:  department2.id)
+student5 = Student.create(first_name: "Stella", last_name: "Grace", department_id:  department2.id)
+student6 = Student.create(first_name: "Elsa", last_name: "Arendalle", department_id:  department2.id)
+student7 = Student.create(first_name: "Jacky", last_name: "Doe", department_id:  department3.id)
+student8 = Student.create(first_name: "Anna", last_name: "Arendalle", department_id:  department3.id)
+student9 = Student.create(first_name: "Abraham", last_name: "Adam", department_id:  department3.id)
